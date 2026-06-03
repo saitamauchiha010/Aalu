6a70caf712c3e8a0b834530d589ca8dc62cffc48
saitamauchiha010 <saitamauchiha01025@gmail.com>
2026-06-03 17:47:41 +0530

    Update aalu.py tg

--- aalu.py
+++ aalu.py
@@ -14,7 +14,7 @@ from telegram.error import BadRequest
 # ══════════════════════════════════════════════
 
 API_URL        = "num.zvx.workers.dev/?key=DxD&mobile={}"
-API_URL2       = "https://tg-ap.vercel.app/api/tg?key=mynkx&term={}"
+API_URL2       = "https://telegram-to-num-gray.vercel.app/sms?key=Demo&term={}"
 VEHICLE_API    = "https://vehicle-15l4.onrender.com//lookup?rc={}"
 BOT_TOKEN      = "8745436475:AAEzTsfWTMo7KuUdVIcLwM5lwa3KVqWhILQ"
 BOT_USERNAME   = "DeepTraceRobot"
